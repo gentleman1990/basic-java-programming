aaee71dc9608656e3c7ddd0b7bbad81c5b22db75
Jakub Socha <Jakub.Socha@LCJ-5CG8113Q45.global.ad>
2019-02-12 15:31:04 +0100

    Update Grocery shop

--- src/main/java/goods/GroceryShop.java
+++ src/main/java/goods/GroceryShop.java
@@ -3,6 +3,10 @@ package goods;
 import goods.fruits.Apple;
 
 public class GroceryShop {
-
+    //create 3 diffrent object Apple with parameters
+    //Jonagold, Red, 10
+    //Ligol, Green, 20
+    //Rubonola, Red, 5
+    //Check in if conditions if color has is the same for each combinations of apple
 
 }
