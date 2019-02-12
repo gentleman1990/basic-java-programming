a10807400b418e93ad2385e80db890ee02c7eba0
Jakub Socha <Jakub.Socha@LCJ-5CG8113Q45.global.ad>
2019-02-12 15:32:34 +0100

    Solution for 1_4 excercise

--- src/main/java/goods/GroceryShop.java
+++ src/main/java/goods/GroceryShop.java
@@ -9,4 +9,20 @@ public class GroceryShop {
     //Rubonola, Red, 5
     //Check in if conditions if color has is the same for each combinations of apple
 
+    public static void main(String[] args) {
+        Apple apple = new Apple("Jonagold", "Red", 10);
+        Apple apple2 = new Apple("Ligol", "Green", 20);
+        Apple apple3 = new Apple("Rubinola", "Red", 5);
+
+        if (apple.getColor() == apple3.getColor()) {
+            System.out.println("Same color 1 and 3");
+        } else if (apple2.getColor() == apple3.getColor()){
+            System.out.println("Same color 2 and 3");
+        } else if (apple.getColor() == apple2.getColor()){
+            System.out.println("Same color 1 and 2");
+        }
+        else {
+            System.out.println("Diffrent color");
+        }
+    }
 }
