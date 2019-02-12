4f2d55dfdd68247ef09509fb1a7ba07f0bb0b979
Jakub Socha <Jakub.Socha@LCJ-5CG8113Q45.global.ad>
2019-02-12 12:05:14 +0100

    Object,class,method

---
+++ src/main/java/test/SimpleClass.java
@@ -0,0 +1,41 @@
+package test;
+
+public class SimpleClass {
+
+    //Fields declaration
+    public int simpleField = 0;
+    public String simpleStringField = "SimpleString";
+
+    //Constructor declaration
+    public SimpleClass(){
+
+    }
+
+    public SimpleClass(int newSimpleField){
+        this.simpleField = newSimpleField;
+    }
+
+    //Methods void type declaration
+    public void simplePublicMethod() {
+        System.out.println("test");
+    }
+
+    private void simplePrivateMethod() {
+        System.out.println("test2");
+    }
+
+    protected void simpleProtectedMethod() {
+        System.out.println("test3");
+    }
+
+    //Methods specific type
+
+    public int simpleReturnTypeMethod(){
+        return simpleField;
+    }
+
+    protected String simpleReturnStringTypeMethod(){
+        return simpleStringField;
+    }
+
+}
